86dfc9ba9c73d4c9dd2a13fe4a7b4ed424571400
JTMoo <jonathan.trefz@t-online.de>
2021-01-29 15:55:30 +0100

    Kommentare und Bugfix

--- Stimulus_Layer.py
+++ Stimulus_Layer.py
@@ -8,7 +8,7 @@ def in_Range(range, value):
     return  tf.math.logical_and(tf.math.greater_equal(value, -range), tf.math.less_equal(value, range))
 
 class StimulusLayer(tf.keras.layers.Layer):
-  def __init__(self, units=32, threshold=20., range=0.1):
+  def __init__(self, units=32, threshold=20., range=0.06):
     super(StimulusLayer, self).__init__()
     self.threshold = threshold
     self.range = range
@@ -16,31 +16,40 @@ class StimulusLayer(tf.keras.layers.Layer):
 
   def build(self, input_shape):
     w_init = tf.initializers.Zeros()
-    # Bekannte Dimensionen finden
+    # create shape from given partially known input_shape
     dims = []
-    dims.append(self.units)
+    
     for dim in input_shape:
         if dim is not None:
             dims.append(dim)
+        else:
+            dims.append(self.units)
     
-    # inital comparison layer
+    # initialization of state variables
     self.last_inputs = tf.Variable(initial_value=w_init(shape=dims, dtype='float32'), shape=input_shape,  trainable=False)
-    self.counter_layer = tf.Variable(initial_value=w_init(shape=dims, dtype='float32'), shape=input_shape, trainable=False)
+    self.counters = tf.Variable(initial_value=w_init(shape=dims, dtype='float32'), shape=input_shape, trainable=False)
 
   def call(self, inputs, training=None):
     trainings_outputs = []
     
+    # training variable is assigned by the keras backend
     if training:
+        # Change shape of state variables according to batch_size (first dimension of shape)
         if tf.shape(inputs)[0] != tf.shape(self.last_inputs)[0]:
             self.last_inputs.assign(tf.zeros(K.shape(inputs)))
-            self.counter_layer.assign(tf.zeros(K.shape(inputs)))
+            self.counters.assign(tf.zeros(K.shape(inputs)))
         
-        self.counter_layer.assign(tf.where(in_Range(self.range, inputs - self.last_inputs), tf.math.add(self.counter_layer, 1), tf.math.multiply(self.counter_layer, 0)))
-        self.counter_layer.assign(tf.where(tf.math.greater_equal(self.counter_layer, 2.*self.threshold), 0., self.counter_layer))
+        # Change counters according to inputs - last_inputs (check if inputs stay the same)
+        # tf.where(condition, true return, false return)
+        # tensorflow internal functions work elementwise for the given inputs
+        self.counters.assign(tf.where(in_Range(self.range, inputs - self.last_inputs), tf.math.add(self.counters, 1), tf.math.multiply(self.counters, 0)))
+        self.counters.assign(tf.where(tf.math.greater_equal(self.counters, 2.*self.threshold), 0., self.counters))
         
         self.last_inputs.assign(inputs)
-        trainings_outputs = tf.where(tf.math.greater_equal(self.counter_layer, self.threshold), 0., inputs)
+        # resistence as returning zero for given input
+        trainings_outputs = tf.where(tf.math.greater_equal(self.counters, self.threshold), 0., inputs)
     
+    # training true => trainings_outputs else inputs
     return K.in_train_phase(trainings_outputs, inputs, training=training)
     
 #local test
